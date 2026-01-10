54b902860bca004358d93122e6954bc672747850
kaka <11076632@qq.com>
2026-01-10 22:59:17 +0800

    feat: accomplish sidebar

--- src/services/ index.ts
+++ src/services/ index.ts
@@ -1,43 +1,43 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://some-domain.com/api/",
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
+	baseURL: "https://some-domain.com/api/",
+	timeout: 1000,
+	headers: { "X-Custom-Header": "foobar" },
 });
 
 // 请求拦截器
 instance.interceptors.request.use(
-  function (config) {
-    // 在发送请求之前做些什么
-    return config;
-  },
-  function (error) {
-    // 对请求错误做些什么
-    return Promise.reject(error);
-  }
+	(config) => {
+		// 在发送请求之前做些什么
+		return config;
+	},
+	(error) => {
+		// 对请求错误做些什么
+		return Promise.reject(error);
+	},
 );
 
 // 响应拦截器
 instance.interceptors.response.use(
-  function (response) {
-    // 对响应数据做点什么
-    return response;
-  },
-  function (error) {
-    // 对响应错误做点什么
-    return Promise.reject(error);
-  }
+	(response) => {
+		// 对响应数据做点什么
+		return response;
+	},
+	(error) => {
+		// 对响应错误做点什么
+		return Promise.reject(error);
+	},
 );
 
 // 封装get方法
 export const get = (url: string, params?: object) => {
-  return instance.get(url, { params });
+	return instance.get(url, { params });
 };
 
 // 封装post方法
 export const post = (url: string, data?: object) => {
-  return instance.post(url, data);
+	return instance.post(url, data);
 };
 
 // export default instance;
